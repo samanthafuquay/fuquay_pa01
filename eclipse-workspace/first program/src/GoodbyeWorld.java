9d3479bb2dcdc674064e30a5b8dc8393cb2f61d9
saman <samanthadeshong@yahoo.com>
2020-01-17 21:35:15 -0500

    goodbyeworld commit

--- eclipse-workspace/first program/src/GoodbyeWorld.java
+++ eclipse-workspace/first program/src/GoodbyeWorld.java
@@ -1,10 +1,9 @@
 
-public class Helloworld {
+public class GoodbyeWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, World!");
-
+		System.out.printf("Goodbye, World!");
 	}
 
 }
